09d227c90ebec6668d0fb854ffd9bc8db1d151df
SwiftVapor <jonathongthayne@gmail.com>
2020-09-09 20:36:50 -0600

    Worked on cards for bit

---
+++ Part2/index.js
@@ -0,0 +1,6 @@
+console.log ('The House Always Wins')
+let id = document.getElementById('idInput');
+let color = document.getElementById('colorInput');
+let setCard = (()=>{
+    let card = document.getElementById
+});
