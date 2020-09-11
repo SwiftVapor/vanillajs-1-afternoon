bdc17b198e01ae95095231fde0236cf0361ce390
SwiftVapor <jonathongthayne@gmail.com>
2020-09-10 21:51:19 -0600

    finished part 2

--- Part2/index.js
+++ Part2/index.js
@@ -1,6 +1,25 @@
 console.log ('The House Always Wins')
-let id = document.getElementById('idInput');
-let color = document.getElementById('colorInput');
+// let id = document.getElementById('idInput');
+// let colorInput = document.getElementById('colorInput');
+
+// let setCard = (()=>{
+//     let card = id.value;
+//     console.log (document.getElementById(idInput.value))
+//     console.log (card)
+//     card.style.color = colorInput.value;
+// });
+
+const idInput2 = document.getElementById('idInput');
+const colorInputJS = document.getElementById('colorInput');
+
+// function setCard() {
+//     const card = document.getElementById(idInput.value);
+//     console.log(card);
+//     card.style.color = colorInput.value;
+//   }
+
 let setCard = (()=>{
-    let card = document.getElementById
+    let card = document.getElementById(idInput2.value);
+    console.log (card)
+    card.style.color = colorInput.value;
 });
